94c55bfc5099da84957393839e96192943713f6e
xymiao <xymiaotec@126.com>
2024-01-01 00:58:16 +0800

    第一章开篇

--- code/chapter01/c01-02-routerhello-jsx/src/main.jsx
+++ code/chapter01/c01-02-routerhello-jsx/src/main.jsx
@@ -1,44 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Route, Link, } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Link, createRoutesFromElements, } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
+const router = createBrowserRouter(
+  createRoutesFromElements(<>
+    <Route path="/" element={(
       <div>
-        <h1>首页</h1>
+        <h1>首页 jsx</h1>
         <div>
           <Link to="/">首页</Link>
           &nbsp;&nbsp;&nbsp;&nbsp;
           <Link to="/about">关于</Link>
         </div>
       </div>
-    ),
-  },
-  {
-    path: "/about",
-    element: (
+    )} />
+    <Route path="about" element={(
       <div>
-        <h1>关于页面</h1>
+        <h1>关于页面 jsx</h1>
         <div>
           <Link to="/">首页</Link>
           &nbsp;&nbsp;&nbsp;&nbsp;
           <Link to="/about">关于</Link>
         </div>
       </div>
-    ),
-  },
-  {
-    path: "*",
-    element: (
-      <div>
-        <h1>404</h1>
-      </div>
-    ),
-  },
-
-]);
+    )} />
+    <Route
+      path="*"
+      element={<div>Not Found</div>} />
+  </>
+  ));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
